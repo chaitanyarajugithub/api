d93959eb03155fadbd9386e2edd23401617a0b12
Chaitanya <44492694+chaitanyarajugithub@users.noreply.github.com>
2019-02-21 17:47:06 +0530

    Add files via upload

---
+++ src/test/java/api/Test/Petclinic_api.java
@@ -0,0 +1,42 @@
+package api.Test;
+
+
+	
+
+
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+
+
+public class Petclinic_api {	
+@Test	
+	public void testResponsecode()
+	{
+		Response resp = RestAssured.get("http://35.154.35.155:8080/petclinic/vets.json");
+		int code = resp.getStatusCode();
+		System.out.println("status code is" +code);
+
+		
+		Assert.assertEquals(code, 200);
+		
+	}
+@Test	
+public void testResponse()
+{
+	Response resp = RestAssured.get("http://35.154.35.155:8080/petclinic/vets.json");
+	String data = resp.asString();
+	System.out.println("Data is" +"\n" + data);
+
+	System.out.println("response time is" + resp.getTime());
+	
+	
+
+}
+}
+
